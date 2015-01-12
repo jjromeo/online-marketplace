231ef620e167b406aceb63fbadac5139ce8591de
Jerome Pratt <jerome.pratt@outlook.com>
2015-01-12 19:20:55 +0000

    created project, added product class which can hold data

---
+++ lib/product.rb
@@ -0,0 +1,8 @@
+class Product
+  attr_accessor :code, :name, :price
+  def initialize(attributes = {})
+    @code = attributes[:code]
+    @name = attributes[:name]
+    @price = attributes[:price]
+  end
+end
