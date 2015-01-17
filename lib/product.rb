99a63dc9e96e37b681e77faf5a8f9455905910ce
Jerome Pratt <jerome.pratt@outlook.com>
2015-01-17 22:45:42 +0000

    made product more flexible - perhaps unnecessarily so, but it let me dry up the code

--- lib/product.rb
+++ lib/product.rb
@@ -1,16 +1,25 @@
 class Product
-  attr_accessor :code, :name, :price
 
+  #ATTRS constant will control which attrs a product must be initialized with
   ATTRS = [:code, :name, :price]
 
   def initialize(attributes = {})
-    @code = attributes[:code]
-    @name = attributes[:name]
-    @price = attributes[:price]
+    set_attrs(attributes)
     validate_attrs
   end
 
   private
+
+  #Sets instance variable for each attribute to its value in attributes hash 
+  #Creates an attr_accessor for that attribute
+  def set_attrs(attributes)
+    ATTRS.each do |attr| 
+      instance_variable_set("@#{attr}", attributes[attr])
+      self.class.send(:attr_accessor, attr)
+    end
+  end
+
+  #Checks all attributes are accounted for
   def validate_attrs
     ATTRS.each do |attr| 
       raise "Error: Missing #{attr} attribute" unless instance_variable_get("@#{attr}") 
